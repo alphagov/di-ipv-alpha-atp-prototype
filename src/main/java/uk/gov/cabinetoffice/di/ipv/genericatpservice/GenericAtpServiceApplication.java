62cf89e7dc2b1ff201bc2a6b5ecd82c2601fd66b
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-06-01 12:36:23 +0100

    Initial commit

---
+++ src/main/java/uk/gov/cabinetoffice/di/ipv/genericatpservice/GenericAtpServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.cabinetoffice.di.ipv.genericatpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericAtpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericAtpServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/gov/cabinetoffice/di/ipv/genericatpservice/GenericAtpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.cabinetoffice.di.ipv.genericatpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericAtpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
